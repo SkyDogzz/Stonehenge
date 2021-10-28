ee8d36d9db66fd8c0f7f23984f3000091e52afb4
SkyDogzz <thomas.stephan@live.fr>
2021-10-29 01:48:39 +0200

    Add controls panel and fix some bugs

--- public/main.js
+++ public/main.js
@@ -1,32 +1,51 @@
 import * as THREE from '../node_modules/three/build/three.module.js'
+import { OrbitControls } from 'https://cdn.skypack.dev/three@0.133.1/examples/jsm/controls/OrbitControls.js';
+import { GUI } from '../node_modules/three/examples/jsm/libs/dat.gui.module.js';
+
+let rotationSpeed = 0.005
+let rotate = true
 
 let axesHelper
+
 let scene, camera, renderer
 let pillarExtGeometry, topExtGeometry, pillarIntGeometry, topIntGeometry, pillarMidGeometry, groundGeometry
 let pillarExtMaterial, topExtMaterial, pillarIntMaterial, topIntMaterial, pillarMidMaterial, groundMaterial
 let pillarExtTexture, topExtTexture, pillarIntTexture, topIntTexture, pillarMidTexture, groundTexture
 let pillarExt, topExt, pillarInt, topInt, pillarMid, ground
 let ambientLight, pointLight
+let controls
 
 init()
 animate()
 
 function init() {
+    renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.shadowMap.enabled = true
+    renderer.shadowMap.type = THREE.BasicShadowMap
+    document.body.appendChild(renderer.domElement);
+
     camera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight, 0.1, 20000);
     camera.position.z = 4500
     camera.position.y = 2500
     camera.lookAt(0, 0, 0)
 
+    controls = new OrbitControls(camera, renderer.domElement);
+    controls.enablePan = false
+    controls.maxPolarAngle = THREE.Math.degToRad(80)
+
     scene = new THREE.Scene();
 
     axesHelper = new THREE.AxesHelper(10000);
+    axesHelper.visible = false
     scene.add(axesHelper);
 
     ambientLight = new THREE.AmbientLight(0xffffff, 0.2)
     scene.add(ambientLight)
 
     pointLight = new THREE.PointLight(0xffffff, 0.6, 0)
-    pointLight.position.set(2000, 6000, 0)
+    pointLight.position.set(3000, 6000, 0)
     pointLight.shadow.mapSize.width = 2048
     pointLight.shadow.mapSize.height = 2048
     pointLight.shadow.camera.near = 0.1
@@ -126,12 +145,35 @@ function init() {
         scene.add(pillarMid)
     }
 
-    renderer = new THREE.WebGLRenderer({ antialias: true });
-    renderer.setPixelRatio(window.devicePixelRatio);
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    renderer.shadowMap.enabled = true
-    renderer.shadowMap.type = THREE.BasicShadowMap
-    document.body.appendChild(renderer.domElement);
+
+    var params = {
+        rotationSpeed: 0.005,
+        switch: true,
+        helper: false
+    };
+
+    const gui = new GUI()
+
+    const cameraFolder = gui.addFolder('Camera')
+    cameraFolder.add(params, "switch").name("Auto Rotation").onChange(() => {
+        rotate = !rotate
+    });
+    cameraFolder.add(params, "rotationSpeed", -0.1,0.1).name('Rotation Speed').onChange((speed) =>{
+        rotationSpeed = speed
+    })
+    cameraFolder.add(params, "helper").name("Axes Helper").onChange(() => {
+        axesHelper.visible = !axesHelper.visible
+    });
+
+    const lightsFolder = gui.addFolder('Lights')
+    const ambientLightFolder = lightsFolder.addFolder('Ambient Light')
+    ambientLightFolder.add(ambientLight, "intensity", 0,1).name('Intensity')
+    const pointLightFolder = lightsFolder.addFolder('Point Light')
+    pointLightFolder.add(pointLight, "intensity", 0,1).name('Intensity')
+    pointLightFolder.add(pointLight.position, "x", -5000,5000).name('X')
+    pointLightFolder.add(pointLight.position, "y", 0,10000).name('Y')
+    pointLightFolder.add(pointLight.position, "z", -5000,5000).name('Z')
+    
 
     window.addEventListener('resize', onWindowResize);
 }
@@ -146,7 +188,7 @@ function onWindowResize() {
 function animate() {
     requestAnimationFrame(animate);
 
-    scene.rotation.y += 0.005
+    scene.rotation.y += rotationSpeed * rotate
 
     renderer.render(scene, camera);
 }
